6a738996f17f83bd8beff82761672c700c89b6e6
dnr2144 <dnr2144@gmail.com>
2021-03-26 17:57:46 +0900

    Jpa Auditing 으로 생성시간 / 수정시간 자동화

--- src/main/java/com/jojoldubook/practice/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojoldubook/practice/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.jojoldubook.practice.springboot.domain.posts;
 
+import com.jojoldubook.practice.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,7 +13,7 @@ import javax.persistence.*;
 // 테이블과 링크될 클래스임을 나타냄
 // 기본값으로 클래스의 카멜케이스 이름을 언더스코어 네이밍르오 매칭(e.g. SalesManager.java -> sales_manager table)
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
     @Id // 해당 테이블의 PK 필드
     @GeneratedValue(strategy = GenerationType.IDENTITY) // PK 생성 규칙. 스프링부트 2.0에서는 GenerationType.IDENTITY를 붙여야 AUTO_INCREMENT 적용
     private Long id;
--- src/test/java/com/jojoldubook/practice/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/jojoldubook/practice/springboot/web/PostsApiControllerTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
