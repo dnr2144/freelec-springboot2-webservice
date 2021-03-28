cfbee37a2adc8e4388c3bf4d2d6b89bde13246c7
dnr2144 <dnr2144@gmail.com>
2021-03-28 11:13:51 +0900

    게시글 수정 기능 추가

--- src/main/java/com/jojoldubook/practice/springboot/web/PostsApiController.java
+++ src/main/java/com/jojoldubook/practice/springboot/web/PostsApiController.java
@@ -1,6 +1,6 @@
 package com.jojoldubook.practice.springboot.web;
 
-import com.jojoldubook.practice.springboot.service.posts.PostsService;
+import com.jojoldubook.practice.springboot.service.PostsService;
 import com.jojoldubook.practice.springboot.web.dto.PostsResponseDto;
 import com.jojoldubook.practice.springboot.web.dto.PostsSaveRequestDto;
 import com.jojoldubook.practice.springboot.web.dto.PostsUpdateRequestDto;
@@ -18,13 +18,13 @@ public class PostsApiController {
         return postsService.save(requestDto);
     }
 
-    @PutMapping("/api/v1/posts/{id}") // update book
-    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
-        return postsService.update(id, requestDto);
-    }
-
     @GetMapping("/api/v1/posts/{id}") // search book
     public PostsResponseDto findById(@PathVariable Long id) {
         return postsService.findById(id);
+    }
+
+    @PutMapping("/api/v1/posts/{id}") // update book
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
     }
 }
