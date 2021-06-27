614ad8db93e5895011e1cf3c8507f74165ad735a
dnr2144 <dnr2144@gmail.com>
2021-06-27 12:00:22 +0900

    some files untracked

---
+++ src/main/java/com/jojoldubook/practice/springboot/config/auth/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.jojoldubook.practice.springboot.config.auth;
+
+import com.jojoldubook.practice.springboot.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.userinfo.CustomUserTypesOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+
+@RequiredArgsConstructor
+@EnableWebSecurity // Spring Security 설정들을 활성화
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf()
+                .disable()
+                .headers().frameOptions().disable() //h2-console 화면을 사용하기 위해 해당 옵션 disable
+                .and()
+                .authorizeRequests()// url별 권한 관리를 설정하는 옵션 시작점. authorizeRequests가 선언되어야만 antMachers 옵션 사용 가능
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**")
+                .permitAll() // antMachers: 권한 곤리 대상을 지정하는 옵션
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                .anyRequest()
+                .authenticated() // 설정된 값들 이외 나머지 URL들을 나타냄
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserServ);
+
+
+    }
+}
+
+
--- src/main/java/com/jojoldubook/practice/springboot/domain/user/Role.java
+++ src/main/java/com/jojoldubook/practice/springboot/domain/user/Role.java
@@ -11,7 +11,7 @@ public enum Role {
     * 스프링 시큐리티에서는 권한 코드에 항상 ROLE_이 앞에 있어야만 한다.
     * 그래서 코드별 키 값을 ROLE_GUEST, ROLE_USER 등으로 지정
     */
-    GUSEST("ROLE_GUEST", "손님"),
+    GUEST("ROLE_GUEST", "손님"),
     USER("ROLE_USER", "일반 사용자");
 
     private final String key;
